5ac2f6621efd22605a51d728a649757549ca5355
Alexis Sanchez <aasanchez@gmail.com>
2021-01-01 19:59:43 +0100

    pulzu checker

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+npm update -g
 npm list -g --depth 0 > packages/npm
 sed -i "s/[├─└── ]//g" packages/npm
 sed -i "/nvm/g" packages/npm
@@ -9,11 +9,10 @@ sed -i '/^$/d' packages/npm
 ( 
 cd packages 
 rm Brewfile*
+brew upgrade
 brew bundle dump
 )
 
 code --list-extensions | xargs -L 1 echo code --install-extension > packages/vscode-packages
-
 vagrant plugin list > packages/vagrant
-
 cp .ssh/known_hosts credentials/.ssh/known_hosts 
--- restore.sh
+++ restore.sh
@@ -13,35 +13,35 @@ stow -D --verbose .
 # done <packages/vscode-packages
 
 # Update internal gitignore
-tree -af  credentials > credentials-files
-tree -afd credentials > credentials-directories
-sed -i '$ d' credentials-files
-sed -i '$ d' credentials-directories
-sed -i '/^$/d' credentials-files
-sed -i '/^$/d' credentials-directories
-sed -i "s/[^A-Za-z0-9._\/]//g" credentials-files
-sed -i "s/[^A-Za-z0-9._\/]//g" credentials-directories
-while read directory; do
-  sed -i "s|^$directory$||g" credentials-files
-done <credentials-directories
-sed -i "s/credentials\///g" credentials-files
-sed -i "s/.git//g" credentials-files
-sed -i "s/README.md//g" credentials-files
-sed -i '/^$/d' credentials-files
-cat credentials-files > .gitignore
-rm credentials-files credentials-directories
+# tree -af  credentials > credentials-files
+# tree -afd credentials > credentials-directories
+# sed -i '$ d' credentials-files
+# sed -i '$ d' credentials-directories
+# sed -i '/^$/d' credentials-files
+# sed -i '/^$/d' credentials-directories
+# sed -i "s/[^A-Za-z0-9._\/]//g" credentials-files
+# sed -i "s/[^A-Za-z0-9._\/]//g" credentials-directories
+# while read directory; do
+#   sed -i "s|^$directory$||g" credentials-files
+# done <credentials-directories
+# sed -i "s/credentials\///g" credentials-files
+# sed -i "s/.git//g" credentials-files
+# sed -i "s/README.md//g" credentials-files
+# sed -i '/^$/d' credentials-files
+# cat credentials-files > .gitignore
+# rm credentials-files credentials-directories
 
-while read secret; do
-  cp -rf credentials/$secret $secret
-done <.gitignore
+# while read secret; do
+#   cp -rf credentials/$secret $secret
+# done <.gitignore
 
 stow .
 
-sudo chmod 0400 ~/.ssh/id_rsa
+# sudo chmod 0400 ~/.ssh/id_rsa
 # eval $(ssh-agent -s); ssh-add -k ~/.ssh/id_rsa
 
 # gpg --import .gpg/gpg.asc
 
-echo ".vscode" >> .gitignore
-echo ".ssh/known_hosts" >> .gitignore
-sort -o  .gitignore .gitignore
+# echo ".vscode" >> .gitignore
+# echo ".ssh/known_hosts" >> .gitignore
+# sort -o  .gitignore .gitignore
