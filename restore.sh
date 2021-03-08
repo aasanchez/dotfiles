5982783e172caf844a056bd6dcd44e30a697f423
Alexis Sanchez <aasanchez@gmail.com>
2021-03-08 21:54:49 +0100

    Update

--- restore.sh
+++ restore.sh
@@ -1,36 +1,46 @@
 #!/usr/bin/env bash
-stow -D --verbose .
 
 (
-cd packages 
-brew bundle
+cd packages || exit
+brew bundle cleanup --force
 rm Brewfile.lock.json
 )
 
+PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+PATH="/usr/local/opt/make/libexec/gnubin:$PATH"
+
+# rm -rf ~/.zshrc
+stow -D --verbose .
+
+mkdir ~/Development
+touch ~/Development/Readme.md
+mkdir ~/Development/Pulzu
+mkdir ~/Development/Zartis
+
 while read extension; do
-    $extension
+    $extension --force
 done <packages/vscode-packages
 
 tree -af  credentials > credentials-files
 tree -afd credentials > credentials-directories
-sed -i '$ d' credentials-files
-sed -i '$ d' credentials-directories
-sed -i '/^$/d' credentials-files
-sed -i '/^$/d' credentials-directories
-sed -i "s/[^A-Za-z0-9._\/]//g" credentials-files
-sed -i "s/[^A-Za-z0-9._\/]//g" credentials-directories
+gsed -i '$ d' credentials-files
+gsed -i '$ d' credentials-directories
+gsed -i '/^$/d' credentials-files
+gsed -i '/^$/d' credentials-directories
+gsed -i "s/[^A-Za-z0-9._\/]//g" credentials-files
+gsed -i "s/[^A-Za-z0-9._\/]//g" credentials-directories
 while read directory; do
-    sed -i "s|^$directory$||g" credentials-files
+    gsed -i "s|^$directory$||g" credentials-files
 done <credentials-directories
-sed -i "s/credentials\///g" credentials-files
-sed -i "s/.git//g" credentials-files
-sed -i "s/README.md//g" credentials-files
-sed -i '/^$/d' credentials-files
+gsed -i "s/credentials\///g" credentials-files
+gsed -i "s/.git//g" credentials-files
+gsed -i "s/README.md//g" credentials-files
+gsed -i '/^$/d' credentials-files
 cat credentials-files > .gitignore
 rm credentials-files credentials-directories
 
 while read secret; do
-    cp -rf credentials/$secret $secret
+    cp -rf credentials/"$secret" "$secret"
 done <.gitignore
 
 stow .
