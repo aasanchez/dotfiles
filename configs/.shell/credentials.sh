c2b9af4c3be8c5a58999c82c5de79f7efcfdef45
Alexis Sánchez <aasanchez@gmail.com>
2023-10-01 21:57:14 +0200

    ✨ feat: Update gitignore and add Code settings and snippets
    - Updated .gitignore to include `.shell/credentials.sh`
    - Added `configs/.shell/credentials.sh` file
    - Added `configs/Library/Application Support/Code/User/settings.json` file with various editor configurations
    - Added `configs/Library/Application Support/Code/User/snippets/shebang.code-snippets` file for Bash shebang snippet

--- configs/.shell/credentials.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export GITHUB_ACCESS_TOKEN="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"
-export GITLAB_ACCESS_TOKEN="glpat-Dm6Emf_syLADNYX8n1Xf"
-export GITLAB_USER="aasanchez"
-export OPENAI_API_KEY="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"
