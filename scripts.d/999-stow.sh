8a0e9ecdf2d74a59c92fb5cff7386821ebf5de1f
Alexis Sánchez <aasanchez@gmail.com>
2023-10-11 15:28:25 +0200

    Gitlab script

--- scripts.d/999-stow.sh
+++ scripts.d/999-stow.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-ignore_files='/Users/aasanchez/Library/CloudStorage/OneDrive-EVASolutionsGroupOy'
-
 cd $HOME/.dotfiles
 
-stow --verbose --ignore="$ignore_files" --restow configs
+stow --restow configs
